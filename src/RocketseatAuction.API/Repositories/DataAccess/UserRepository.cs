b4f87836957d3c29c02fe5277bc97ff3bc93e3c4
Pedro Kawano <pedrolucas_kawano@hotmail.com>
2024-02-12 04:44:51 -0300

    first commit

---
+++ src/RocketseatAuction.API/Repositories/DataAccess/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using RocketseatAuction.API.Contracts;
+using RocketseatAuction.API.Entities;
+
+namespace RocketseatAuction.API.Repositories.DataAccess;
+
+public class UserRepository : IUserRepository
+{
+    private readonly RocketseatAuctionDbContext _dbcontext;
+
+    public UserRepository(RocketseatAuctionDbContext dbcontext) => _dbcontext = dbcontext;
+
+    public bool ExistUserWithEmail(string email)
+    {
+        return _dbcontext.Users.Any(user => user.Email.Equals(email));
+    }
+
+    public User GetUserByEmail(string email) => _dbcontext.Users.First(user => user.Email.Equals(email));
+}
